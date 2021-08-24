8075d9b76305b8b7319b788403368b69105bdab8
Vu Thanh Long <longvt@flextech.vn>
2021-08-25 02:07:09 +0700

    fix: axios

--- src/libs/axios.js
+++ src/libs/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: process.env.API_HOST + process.env.API_PATH,
+    baseURL: process.env.API_PATH,
     headers: {
         'content-type': 'application/json',
     },
